0da0b51666601b9bf86f65991e50fa67275ade05
Origogi <rlawjdxo1212@naver.com>
2025-01-30 00:02:45 +0900

    Fix parameter value

--- lib/main.dart
+++ lib/main.dart
@@ -37,31 +37,34 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          // Pokemon Card (as overlay)
-          TiltCard(),
-          // Pokemon Image with positioning
-          Positioned(
-            bottom: MediaQuery.of(context).size.height * 0.3, // 10%
-            left: MediaQuery.of(context).size.width * 0.4, // 50%
-            child: Transform(
-              transform: Matrix4.identity()
-                ..translate(
-                    -125.0, 0, 100.0), // translateX(-50%) and translateZ(100px)
-              child: SizedBox(
-                width: 300,
-                height: 300,
-                child: Image.network(
-                  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png',
-                  fit: BoxFit.contain,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+      body: TiltCard(),
     );
+    // return Scaffold(
+    //   body: Stack(
+    //     children: [
+    //       // Pokemon Card (as overlay)
+    //       TiltCard(),
+    //       // Pokemon Image with positioning
+    //       Positioned(
+    //         bottom: MediaQuery.of(context).size.height * 0.3, // 10%
+    //         left: MediaQuery.of(context).size.width * 0.4, // 50%
+    //         child: Transform(
+    //           transform: Matrix4.identity()
+    //             ..translate(
+    //                 -125.0, 0, 100.0), // translateX(-50%) and translateZ(100px)
+    //           child: SizedBox(
+    //             width: 300,
+    //             height: 300,
+    //             child: Image.network(
+    //               'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png',
+    //               fit: BoxFit.contain,
+    //             ),
+    //           ),
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 }
 
@@ -70,7 +73,8 @@ class TiltCard extends StatefulWidget {
   _TiltCardState createState() => _TiltCardState();
 }
 
-class _TiltCardState extends State<TiltCard> with SingleTickerProviderStateMixin {
+class _TiltCardState extends State<TiltCard>
+    with SingleTickerProviderStateMixin {
   double _tiltX = 0.0;
   double _tiltY = 0.0;
   late AnimationController _controller;
@@ -96,8 +100,8 @@ class _TiltCardState extends State<TiltCard> with SingleTickerProviderStateMixin
       _tiltY = (position.dx - 125) / 20; // 최대 기울기 조정
 
       // 최대 기울기 제한
-      _tiltX = _tiltX.clamp(-20.0, 20.0); // maxTilt
-      _tiltY = _tiltY.clamp(-20.0, 20.0); // maxTilt
+      _tiltX = _tiltX.clamp(-10.0, 10.0); // maxTilt
+      _tiltY = _tiltY.clamp(-10.0, 10.0); // maxTilt
     });
   }
 
@@ -130,54 +134,81 @@ class _TiltCardState extends State<TiltCard> with SingleTickerProviderStateMixin
             return Transform(
               transform: Matrix4.identity()
                 ..setEntry(3, 2, 0.001) // 원근감 추가
-                ..rotateX(_tiltX * (pi / 180) * (1 - _controller.value)) // X축 회전
-                ..rotateY(_tiltY * (pi / 180) * (1 - _controller.value)), // Y축 회전
+                ..rotateX(
+                    _tiltX * (pi / 180) * (1 - _controller.value)) // X축 회전
+                ..rotateY(
+                    _tiltY * (pi / 180) * (1 - _controller.value)), // Y축 회전
               alignment: FractionalOffset.center,
-              child: Container(
-                width: 250,
-                height: 400,
-                decoration: BoxDecoration(
-                  color: Colors.black.withOpacity(0.75),
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(
-                    color: const Color(0xFFFFC83C),
-                    width: 10,
-                  ),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.black,
-                      blurRadius: 200,
-                      offset: Offset(0, 50),
-                      spreadRadius: -25,
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Container(
+                    width: 250,
+                    height: 400,
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.75),
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(
+                        color: const Color(0xFFFFC83C),
+                        width: 10,
+                      ),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 200,
+                          offset: Offset(0, 50),
+                          spreadRadius: -25,
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                clipBehavior: Clip.antiAlias,
-                child: const Padding(
-                  padding: EdgeInsets.only(left: 16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 10),
-                      Text(
-                        'Pikachu',
-                        style: TextStyle(
-                          fontFamily: 'Silkscreen',
-                          fontSize: 24,
-                          color: Colors.white,
-                        ),
+                    clipBehavior: Clip.antiAlias,
+                    child: const Padding(
+                      padding: EdgeInsets.only(left: 16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 10),
+                          Text(
+                            'Pikachu',
+                            style: TextStyle(
+                              fontFamily: 'Silkscreen',
+                              fontSize: 24,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            '65HP',
+                            style: TextStyle(
+                              fontFamily: 'Silkscreen',
+                              fontSize: 16,
+                              color: Color(0xFFFFC83C),
+                            ),
+                          ),
+                        ],
                       ),
-                      Text(
-                        '65HP',
-                        style: TextStyle(
-                          fontFamily: 'Silkscreen',
-                          fontSize: 16,
-                          color: Color(0xFFFFC83C),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
+                  Positioned(
+                    bottom:
+                        MediaQuery.of(context).size.height * 0.01, // bottom: 10%
+                    left: 150, // 카드 너비(250)의 절반
+                    child: Transform(
+                      transform: Matrix4.identity()
+                        ..setEntry(3, 2, 0.001) // 원근감
+                        ..translate(-180.0, 0.0,
+                            50.0), // translateX(-50%) translateZ(100px)
+                      alignment: FractionalOffset.center,
+                      child: Container(
+                        width: 300, // 원본 CSS와 동일한 크기
+                        height: 300,
+                        child: Image.network(
+                          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png',
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             );
           },
